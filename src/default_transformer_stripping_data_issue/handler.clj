432ed6b3011d2eda2e193a550e6e8062fe932a80
Matt Russell <matthew.russell76@gmail.com>
2018-09-12 14:13:23 +0100

    Resolved.

--- src/default_transformer_stripping_data_issue/handler.clj
+++ src/default_transformer_stripping_data_issue/handler.clj
@@ -1,12 +1,16 @@
 (ns default-transformer-stripping-data-issue.handler
   (:require
    [clojure.spec.alpha :as s]
+   [clojure.pprint :refer [pprint]]
+   [compojure.api.coercion.core :as cc]
    [compojure.api.coercion.spec :as spec-coercion]
    [compojure.api.sweet :refer [api context resource routes]]
+   [muuntaja.core :as m]
    [ring.util.http-response :refer [bad-request ok]]
    [spec-tools.core :as stc]
    [spec-tools.spec :as spec]
-   [spec-tools.transform :as stt]))
+   [spec-tools.transform :as stt]
+   [spec-tools.swagger.core :as swagger]))
 
 (s/def :g/sp spec/keyword?)
 (s/def :g/sn string?)
@@ -28,7 +32,6 @@
                      (s/map-of
                       #{:g/sn
                         :g/bt
-                        :g/sp
                         :p/when
                         :p/who
                         :p/how
@@ -42,7 +45,9 @@
                                    :p/how
                                    :p/why
                                    :p/when])
-                     (s/map-of #{:p/when
+                     (s/map-of #{:g/sp
+                                 :g/cn
+                                 :p/when
                                  :p/who
                                  :p/how
                                  :p/why}
@@ -52,7 +57,17 @@
 
 (s/def ::update (stc/spec (s/or :v-a ::variant-a :v-b ::variant-b)))
 
-;;; hacks to workaround stripping spec-data stripping behaviour in compojure.api
+(defn expect-request-validation-to-fail-on-extra-keys [request]
+  (bad-request {:message (str "Expecting request validation to fail with: "
+                              "compojure.api.exception/request-validation"
+                              "but did not")
+                :body-params (:body-params request)}))
+
+;;; transformers that dont' apply key stripping decoders
+;;;
+;;; (these functions are otherwise verbatim copies of
+;;; compojure.api.coercion.spec/{string-transformer,json-transformer})
+
 (def string-transformer
   (stc/type-transformer
     {:name :string
@@ -63,8 +78,7 @@
 (def json-transformer
   (stc/type-transformer
     {:name :json
-     :Decoders
-     stt/json-type-decoders
+     :decoders stt/json-type-decoders
      :encoders stt/json-type-encoders
      :default-encoder stt/any->any}))
 
@@ -73,14 +87,11 @@
         options (-> spec-coercion/default-options
                     (assoc-in
                      [:body :formats]
-                     (zipmap mimetypes (repeat json-transformer)))
+                     {"application/json" json-transformer
+                     "application/msgpack" json-transformer
+                     "application/x-yaml" json-transformer})
                     (assoc-in [:body :string :default] string-transformer))]
     (spec-coercion/create-coercion options)))
-
-(defn expect-request-validation-to-fail [request]
-  (println "BODY  PARAMS:")
-  (prn (get request :body-params))
-  (bad-request "Expecting request validation to fail"))
 
 (defn keyword-coercion [request]
   (ok {:message "Keywords coerced in body-params ok"
@@ -88,18 +99,19 @@
 
 (def custom-coercion (non-stripping-spec-keys-coercion))
 
+(defmethod cc/named-coercion ::spec [_] custom-coercion)
+
 (def app
-  (api
-    {:swagger
-     {:ui "/"
-      :spec "/swagger.json"
+  (api   
+   {:swagger
+    {:ui "/"
+     :spec "/swagger.json"
       :data {:info {:title "Default-transformer-stripping-data-issue"
                     :description "Compojure Api example"}
              :tags [{:name "api", :description "some apis"}]}}}
     (routes
      (context "/kw-test-custom-spec-coercion" []
-       :coercion custom-coercion
-       ;; :coercion :spec
+       :coercion ::spec
        (resource
         {:post
          {:x-name ::kw-test-custom-spec-coercion
@@ -113,19 +125,19 @@
           :parameters {:body-params ::kw-test}
           :handler keyword-coercion}}))     
      (context "/solution-1" []
-       :coercion (non-stripping-spec-keys-coercion)
+       :coercion ::spec
        (resource
         {:post
          {:x-name ::update-data-custom-coercion-fix
           :parameters {:body-params ::update}
-          :handler expect-request-validation-to-fail}}))
+          :handler expect-request-validation-to-fail-on-extra-keys}}))
      (context "/issue" []
        :coercion :spec
        (resource
         {:post
          {:x-name ::update-data-failing
           :parameters {:body-params ::update}
-          :handler expect-request-validation-to-fail}})))))
+          :handler expect-request-validation-to-fail-on-extra-keys}})))))
 
 
 
